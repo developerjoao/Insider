8ef6063352e241b1ce355073f5b0ce08e82fc178
Joao <jgazbral@gmail.com>
2018-11-18 19:04:53 +0000

    inicio das classes

--- Insider/src/User.java
+++ Insider/src/User.java
@@ -7,5 +7,44 @@
  *
  */
 public class User {
-
+	private String nome;
+	private int id;
+	private String email;
+	private String domain;
+	private String role;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
